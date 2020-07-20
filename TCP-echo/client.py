5b23fc3266168584c535263b6d62f68e3f7039a2
Lulin <kistssy+dev@gmail.com>
2020-07-20 20:10:23 +0900

    Implement TCP echo code

---
+++ TCP-echo/client.py
@@ -0,0 +1,34 @@
+import socket
+
+server_ip = '127.0.0.1'
+server_port = 50000
+
+# 통신용 소켓을 생성한다.
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 서버에 연결한다.
+client_sock.connect((server_ip, server_port))
+
+# 서버에 연결했음을 알린다.
+print('[TCP 클라이언트] 서버 연결: {}:{}'.format(server_ip, server_port))
+
+while True:
+    # 송신할 데이터를 입력한다.
+    buffer = input('Send: ')
+    if buffer == '/exit':
+        break
+
+    # 데이터를 송신한다.
+    retval = client_sock.send(buffer.encode('utf-8'))
+
+    # 데이터를 수신한다.
+    buffer = client_sock.recv(1024)
+
+    # 수신한 데이터를 출력한다.
+    print('Recv: {}'.format(buffer.decode('utf-8'), end='\n\n'))
+
+# 통신용 소켓을 닫는다.
+client_sock.close()
+
+print('프로그램을 종료합니다.')
+exit(0)
---
+++ TCP-echo/server.py
@@ -0,0 +1,46 @@
+import socket
+
+server_port = 50000
+
+# 서버 대기용 소켓을 생성한다.
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Local IP 주소와 Local Port 번호를 설정한다.
+server_sock.bind(('', server_port))
+
+# 서버 대기용 소켓을 LISTENING 상태로 변경한다.
+server_sock.listen()
+
+while True:
+    # 클라이언트의 접속을 기다린다.
+    client_sock, addr = server_sock.accept()
+
+    # 클라이언트가 접속하면 정보를 출력한다.
+    print('\n[TCP 서버] 클라이언트 접속: {}:{}'.format(addr[0], addr[1]))
+
+    while True:
+        # 데이터를 수신한다.
+        try:
+            buffer = client_sock.recv(1024)
+        except ConnectionResetError as e:
+            print(e)
+            break
+
+        # 빈 문자열을 수신하면 서버를 종료한다.
+        if not buffer:
+            break
+
+        # 수신한 데이터를 출력한다.
+        print('[TCP/{}:{}] {}'.format(addr[0], addr[1], buffer.decode('utf-8')))
+
+        # 수신한 데이터를 그대로 송신한다.
+        retval = client_sock.send(buffer)
+
+    # 통신용 소켓을 닫는다.
+    client_sock.close()
+
+    # 클라이언트의 종료를 알린다.
+    print('[TCP 서버] 클라이언트 종료: {}:{}'.format(addr[0], addr[1]))
+
+# 서버 대기용 소켓을 닫는다.
+server_sock.close()
